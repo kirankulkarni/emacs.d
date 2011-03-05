5e594e3137a9f425e066c9c732caf628e2d260a3
Kiran <kk.questworld@gmail.com>
2011-03-05 17:43:45 +0530

    Added python-mode-config file

---
+++ configurations/python-mode-config.el
@@ -0,0 +1,11 @@
+;; Configuring Python, pymacs, ropemacs
+
+(require 'pymacs)
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(pymacs-load "ropemacs" "rope-")
+
+(setq ropemacs-enable-autoimport t)
