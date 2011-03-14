ab6bc9eddef88bf5d01a504696d9896f10e29f0f
Kiran <kk.questworld@gmail.com>
2011-03-14 15:34:07 +0530

    Configured Flymake

---
+++ configurations/flymake-config.el
@@ -0,0 +1,22 @@
+;; Flymake Config
+(when (load "flymake" t) 
+  (defun flymake-pyflakes-init () 
+     ; Make sure it's not a remote buffer or flymake would not work
+     (let* ((temp-file (flymake-init-create-temp-buffer-copy 
+                         'flymake-create-temp-inplace)) 
+             (local-file (file-relative-name 
+                          temp-file 
+                          (file-name-directory buffer-file-name))))
+	(list "pycheckers" (list local-file)))) 
+  (add-to-list 'flymake-allowed-file-name-masks 
+               '("\\.py\\'" flymake-pyflakes-init)))
+
+
+;; From Vedang
+(load-library "flymake-cursor")
+(global-set-key [f2] 'flymake-goto-prev-error)
+(global-set-key [f3] 'flymake-goto-next-error)
+
+;; End of config
+(provide 'flymake-config)
+ 
