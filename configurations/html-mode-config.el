5dc79b4f96130d01d91e5bc22c854ebeae7a3e2b
Kiran Kulkarni <kiran@infinitelybeta.com>
2011-12-28 16:06:39 +0530

    Paredit mode and html doesn't go well

--- configurations/html-mode-config.el
+++ configurations/html-mode-config.el
@@ -1,15 +1,15 @@
 ;;; HTML mode configuration
 
 ;;; paredit configuration
-(defun turn-on-html-paredit ()
-  "Turns Paredit on and sets html-mode map"
-  (paredit-mode t)
-  (define-key html-mode-map "<" 'paredit-open-angled)
-  (define-key html-mode-map ">" 'paredit-close-angled-and-newline)
-  (define-key html-mode-map "{" 'paredit-open-curly)
-  (define-key html-mode-map "}" 'paredit-close-curly-and-newline))
+;; (defun turn-on-html-paredit ()
+;;   "Turns Paredit on and sets html-mode map"
+;;   (paredit-mode t)
+;;   (define-key html-mode-map "<" 'paredit-open-angled)
+;;   (define-key html-mode-map ">" 'paredit-close-angled-and-newline)
+;;   (define-key html-mode-map "{" 'paredit-open-curly)
+;;   (define-key html-mode-map "}" 'paredit-close-curly-and-newline))
 
-(add-hook 'html-mode-hook 'turn-on-html-paredit)
+;; (add-hook 'html-mode-hook 'turn-on-html-paredit)
 
 ;; (eval-after-load 'html-mode
 ;;   '(progn (define-key html-mode-map "<" 'paredit-open-angled)
