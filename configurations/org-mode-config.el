289654a28e4a82bdc6aee80fa9d2a138b13eab5c
Kiran Kulkarni <kk.questworld@gmail.com>
2012-03-03 03:47:53 +0530

    Added Properties configuration for org-mode

--- configurations/org-mode-config.el
+++ configurations/org-mode-config.el
@@ -84,6 +84,18 @@
 ;;; modify your list of tags: uncomment the following command
 ;; (setq org-fast-tag-selection-single-key t)
 
+
+;; ---------------
+;; PROPERTIES
+;; ---------------
+;;; global properties copied inspired by Bernt Hansen
+(setq org-global-properties
+      (quote (("Effort_ALL" . "0:10 0:30 1:00 2:00 3:00 4:00 5:00 6:00 7:00 8:00 0:00")
+              ("STYLE_ALL" . "habit")))
+      org-columns-default-format
+      "%80ITEM(Task) %Effort(Effort){:} %CLOCKSUM")
+
+
 ;; Calc package is needed for making spreadsheets
 (require 'calc)
 
