2038d52a199f59d3cfdcbea86dc1f66a4bb5bded
Kiran Kulkarni <kk.questworld@gmail.com>
2012-04-09 21:06:37 +0530

    Swap windows functionality

--- configurations/util-functions.el
+++ configurations/util-functions.el
@@ -50,5 +50,20 @@
 (defun turn-on-paredit ()
   (paredit-mode t))
 
+;;; Copied from vedang.
+(defun transpose-windows (arg)
+  "Transpose the buffers shown in two windows."
+  (interactive "p")
+  (let ((selector (if (>= arg 0) 'next-window 'previous-window)))
+    (while (/= arg 0)
+      (let ((this-win (window-buffer))
+            (next-win (window-buffer (funcall selector))))
+        (set-window-buffer (selected-window) next-win)
+        (set-window-buffer (funcall selector) this-win)
+        (select-window (funcall selector)))
+      (setq arg (if (plusp arg) (1- arg) (1+ arg))))))
+
+(global-set-key (kbd "C-x M-s") 'transpose-windows)
+
 ;;End of the 
 (provide 'util-functions)
