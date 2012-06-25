bc870b91f73ad104963dc570399618bba01d1ca5
Kiran Kulkarni <kk.questworld@gmail.com>
2012-06-25 10:03:39 +0530

    Removes no-easy-keys minor mode.

--- init.el
+++ init.el
@@ -66,32 +66,6 @@
 ;; Require my customizations
 (require 'kk-customizations)
 
-;; No EASY KEYS
-(defvar no-easy-keys-minor-mode-map (make-keymap)
-  "no-easy-keys-minor-mode keymap.")
-(let ((f (lambda (m)
-           `(lambda () (interactive)
-              (message (concat "No! use " ,m " instead."))))))
-  (dolist (l '(("<left>" . "C-b") ("<right>" . "C-f") ("<up>" . "C-p")
-               ("<down>" . "C-n")
-               ("<C-left>" . "M-f") ("<C-right>" . "M-b") ("<C-up>" . "M-{")
-               ("<C-down>" . "M-}")
-               ("<M-left>" . "M-f") ("<M-right>" . "M-b") ("<M-up>" . "M-{")
-               ("<M-down>" . "M-}")
-               ("<delete>" . "C-d") ("<C-delete>" . "M-d")
-               ("<M-delete>" . "M-d") ("<next>" . "C-v") ("<C-next>" . "M-x <")
-               ("<prior>" . "M-v") ("<C-prior>" . "M-x >")
-               ("<home>" . "C-a") ("<C-home>" . "M->")
-               ("<C-home>" . "M-<") ("<end>" . "C-e") ("<C-end>" . "M->")))
-    (define-key no-easy-keys-minor-mode-map
-      (read-kbd-macro (car l)) (funcall f (cdr l)))))
-(define-minor-mode no-easy-keys-minor-mode
-  "A minor mode that disables the arrow-keys, pg-up/down, delete
-  and backspace."  t " no-easy-keys"
-
-  'no-easy-keys-minor-mode-map :global t)
-(no-easy-keys-minor-mode 1)
-
 ;;; Start emacs server if it is not running
 (load "server")
 (unless (server-running-p)
