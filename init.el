f443df03011cc178e599161c8bf60bb7d015a5f0
Kiran Kulkarni <kk.questworld@gmail.com>
2012-07-09 22:12:15 +0530

    Switch to anonymous pro and adjust height for retina display

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 98 :width normal)))))
+ '(default ((t (:family "Anonymous Pro" :foundry "unknown" :slant normal :weight normal :height 130 :width normal)))))
 
 (show-paren-mode 1) ;;Show paren matching
 
