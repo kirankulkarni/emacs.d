1360f65726d0e4686b317296568d3c7cf3574da4
Kiran Kulkarni <kk.questworld@gmail.com>
2012-10-09 00:13:38 +0530

    Upgrade to latest yasnippet
    JIT is introduced in the new release.
    If your yasnippet database is large you must use this setting.

--- configurations/yasnippet-config.el
+++ configurations/yasnippet-config.el
@@ -1,8 +1,9 @@
 (require 'yasnippet)
-(yas/initialize)
 
-(setq yas/root-directory (concat dotfiles-etc-dir "snippets/"))
+;; (setq yas/root-directory (concat dotfiles-etc-dir "snippets/"))
 
 ;; Load the snippets
-(yas/load-directory (concat dotfiles-etc-dir "snippets/"))
+;; (yas/load-directory (concat dotfiles-etc-dir "snippets/"))
+(yas-global-mode 1)
+;; (add-to-list 'ac-sources 'ac-source-yasnippet)
 (provide 'yasnippet-config)
