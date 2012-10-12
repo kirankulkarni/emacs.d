8c5e29e1849cfe14c7e6d07dc6062a9d1d3d19c7
Kiran Kulkarni <kk.questworld@gmail.com>
2012-10-13 04:03:01 +0530

    Add function to jump between source and header file in ObjC

--- configurations/objc-mode-config.el
+++ configurations/objc-mode-config.el
@@ -21,9 +21,31 @@
     (anything-other-buffer '(anything-c-source-objc-headline)
                            "*ObjC Headline*")))
 
-(global-set-key "\C-xp" 'objc-headline)
+(defun objc-in-header-file ()
+  (let* ((filename (buffer-file-name))
+         (extension (car (last (split-string filename "\\.")))))
+    (string= "h" extension)))
 
-;;; Xcode document view setting
+(defun objc-jump-to-extension (extension)
+  (let* ((filename (buffer-file-name))
+         (file-components (append (butlast (split-string filename
+                                                         "\\."))
+                                  (list extension))))
+    (find-file (mapconcat 'identity file-components "."))))
+
+(defun objc-jump-between-header-source ()
+  (interactive)
+  (if (objc-in-header-file)
+      (objc-jump-to-extension "m")
+    (objc-jump-to-extension "h")))
+
+(defun objc-mode-customizations ()
+  (turn-on-paredit)
+  (define-key objc-mode-map (kbd "C-x p") 'objc-headline)
+  (define-key objc-mode-map (kbd "C-c t") 'objc-jump-between-header-source))
+
+(add-hook 'objc-mode-hook 'objc-mode-customizations)
+;;; xcode document view setting
 (require 'xcode-document-viewer)
 (setq xcdoc:document-path "/Users/kiran/Library/Developer/Shared/Documentation/DocSets/com.apple.adc.documentation.AppleiOS5_1.iOSLibrary.docset")
 (setq xcdoc:open-w3m-other-buffer t)
