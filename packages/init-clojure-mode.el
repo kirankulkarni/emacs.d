019a1c98958ff6b78a54e09ae9a1cdab7dad89e6
Kiran Kulkarni <kk.questworld@gmail.com>
2015-06-19 00:33:43 +0530

    Fix midje test jump code

--- packages/init-clojure-mode.el
+++ packages/init-clojure-mode.el
@@ -23,41 +23,58 @@
                     nil))))))
 
 
-(defun midje-test-for (namespace)
-  (let* ((namespace (clojure-underscores-for-hyphens namespace))
+(defun icl/clojure-underscores-for-hyphens (namespace)
+  "Replace all hyphens in NAMESPACE with underscores."
+  (replace-regexp-in-string "-" "_" namespace))
+
+
+(defun icl/midje-test-for (namespace)
+  (let* ((namespace (icl/clojure-underscores-for-hyphens namespace))
          (segments (split-string namespace "\\."))
          (test-segments (append (list "test") segments)))
     (mapconcat 'identity test-segments "/")))
 
 
-(defun midje-jump-to-test ()
+(defun icl/midje-jump-to-test ()
   "Jump from implementation file to test."
   (interactive)
   (find-file (format "%s/%s_test.clj"
                      (file-name-as-directory
                       (locate-dominating-file buffer-file-name "src/"))
-                     (midje-test-for (clojure-find-ns)))))
+                     (icl/midje-test-for (clojure-find-ns)))))
 
 
-(defun midje-implementation-for (namespace)
-  (let* ((namespace (clojure-underscores-for-hyphens namespace))
-         (segments (split-string (replace-regexp-in-string "_test" "" namespace) "\\.")))
+(defun icl/midje-implementation-for (namespace)
+  (let* ((namespace (icl/clojure-underscores-for-hyphens namespace))
+         (segments (split-string (replace-regexp-in-string "_test"
+                                                           ""
+                                                           namespace)
+                                 "\\.")))
     (mapconcat 'identity segments "/")))
 
 
-(defun midje-jump-to-implementation ()
+(defun icl/midje-jump-to-implementation ()
   "Jump from midje test file to implementation."
   (interactive)
   (find-file (format "%s/src/%s.clj"
                      (locate-dominating-file buffer-file-name "src/")
-                     (midje-implementation-for (clojure-find-package)))))
+                     (icl/midje-implementation-for (clojure-find-ns)))))
 
 
-(defun midje-jump-between-tests-and-code ()
+(defun icl/clojure-in-tests-p ()
+  "Check whether the current file is a test file.
+  Two checks are made - whether the namespace of the file has the
+  word test in it and whether the file lives under the test/
+  directory."
+  (or (string-match-p "test\." (clojure-find-ns))
+      (string-match-p "/test" (buffer-file-name))))
+
+
+(defun icl/midje-jump-between-tests-and-code ()
   (interactive)
-  (if (clojure-in-tests-p)
-      (midje-jump-to-implementation)
-    (midje-jump-to-test)))
+  (if (icl/clojure-in-tests-p)
+      (icl/midje-jump-to-implementation)
+    (icl/midje-jump-to-test)))
 
 
 
@@ -78,7 +95,7 @@
      (remove-hook 'clojure-mode-hook
                   'clojure-test-maybe-enable)
      (define-key clojure-mode-map (kbd "C-c t")
-       'midje-jump-between-tests-and-code)))
+       'icl/midje-jump-between-tests-and-code)))
 
 
 (provide 'init-clojure-mode)
