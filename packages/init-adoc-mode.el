1858f2f95fe8181c27615144360c9f8964e73551
Kiran Kulkarni <kiran@helpshift.com>
2020-02-21 15:18:55 +0530

    Remove initialization files for older packages

--- packages/init-adoc-mode.el
+++
@@ -1,7 +0,0 @@
-
-(add-to-list 'auto-mode-alist (cons "\\.adoc\\'" 'adoc-mode))
-
-(add-hook 'adoc-mode-hook (lambda() (buffer-face-mode t)))
-
-
-(provide 'init-adoc-mode)
--- packages/init-erlang-mode.el
+++
@@ -1,9 +0,0 @@
-
-(add-hook 'erlang-shell-mode-hook
-          (lambda ()
-            ;; add some Distel bindings to the Erlang shell
-            (dolist (spec distel-shell-keys)
-              (define-key erlang-shell-mode-map (car spec) (cadr spec)))))
-
-
-(provide 'init-erlang)
